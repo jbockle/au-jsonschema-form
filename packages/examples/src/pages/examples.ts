17f23c5e8bfe9333601f8ee7044d4c9cc48d19e0
jbockle <otakujb@gmail.com>
2020-09-13 09:20:40 -0400

    docs: add example's id to route params

--- packages/examples/src/pages/examples.ts
+++ packages/examples/src/pages/examples.ts
@@ -1,14 +1,17 @@
 import { observable, inject, TaskQueue } from 'aurelia-framework';
+import { NavigationInstruction, RouteConfig, Router } from 'aurelia-router';
 import { EventAggregator } from 'aurelia-event-aggregator';
 
 import { examples } from '../examples';
 import { Example, ExampleCtor } from '../examples/example';
 
-@inject(TaskQueue, EventAggregator)
+
+@inject(TaskQueue, EventAggregator, Router)
 export class Examples {
   public constructor(
     private _tasks: TaskQueue,
     private _events: EventAggregator,
+    private _router: Router,
   ) { }
 
   @observable
@@ -17,10 +20,16 @@ export class Examples {
   public examples = examples;
 
   @observable
-  public selectedExample?: Example;
+  public selectedExample?: ExampleCtor;
 
   @observable
   public example?: Example | null;
+
+  public activate(params: { id?: string }, _routeConfig: RouteConfig, _navigationInstruction: NavigationInstruction): void {
+    if (params.id && this.selectedExample?.id !== params.id) {
+      this.selectedExample = this.examples.find(e => e.id === params.id);
+    }
+  }
 
   public bind(): void {
     this._events.subscribe('value-updated', () => {
@@ -32,9 +41,13 @@ export class Examples {
     this.example = null;
 
     if (example) {
+      this._router.navigateToRoute('examples', { id: example.id });
       this._tasks.queueMicroTask(() => {
         this.example = new example();
       });
+    }
+    else {
+      this._router.navigateToRoute('examples', {});
     }
   }
 }
