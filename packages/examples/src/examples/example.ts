7e7392a54956997c8058accd64dd0865b75e15f1
jbockle <otakujb@gmail.com>
2020-09-13 09:00:43 -0400

    docs: add default example value

--- packages/examples/src/examples/example.ts
+++ packages/examples/src/examples/example.ts
@@ -1,7 +1,7 @@
 import { JsonSchema, UISchema, FormOptions } from '@aujsf/core';
 
 export abstract class Example {
-  public value: any;
+  public value: any = {};
   public abstract schema: JsonSchema;
   public uiSchema: UISchema = {};
   public options: FormOptions = {};
